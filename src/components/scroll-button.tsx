ee207ac5d831320bc69cb5733125a50979d1c959
Joao Hildebrandt <tools@joaohildebrandt.com>
2026-01-11 02:08:51 +0000

    fix: cursor pointer to scroll button

--- src/components/scroll-button.tsx
+++ src/components/scroll-button.tsx
@@ -6,21 +6,21 @@ import type { PropsWithChildren } from "react";
 import { Button } from "./ui/button";
 
 type ScrollLinkProps = {
-	targetId: string;
+  targetId: string;
 } & PropsWithChildren;
 
 export const ScrollLink = ({ children, targetId }: ScrollLinkProps) => {
-	const handleScroll: React.MouseEventHandler<HTMLButtonElement> = () => {
-		const elem = document.getElementById(targetId);
-		window.scrollTo({
-			top: elem?.getBoundingClientRect().top,
-			behavior: "smooth",
-		});
-	};
+  const handleScroll: React.MouseEventHandler<HTMLButtonElement> = () => {
+    const elem = document.getElementById(targetId);
+    window.scrollTo({
+      top: elem?.getBoundingClientRect().top,
+      behavior: "smooth",
+    });
+  };
 
-	return (
-		<Button variant="link" onClick={handleScroll}>
-			{children}
-		</Button>
-	);
+  return (
+    <Button className="cursor-pointer" variant="link" onClick={handleScroll}>
+      {children}
+    </Button>
+  );
 };
